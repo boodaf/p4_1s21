9e8cb29b428d63d2aba10fec251100a9801a578e
boodaf <144178847+boodaf@users.noreply.github.com>
2023-09-26 06:53:22 -0600

    Add files via upload
    P4_1S21

---
+++ P4-1S21.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class p3_1s21{
+   public static void main(string[]args) {
+      Scanner teclado= new scanner(System.in);
+      String cadena;
+      Syatem.out.printLn("Ingresa tu nombre");
+      cadena =teclado.nextLine();
+      System.out.println("Bienvenido"+ cadena + ", a la clase") 
+   }
+}
